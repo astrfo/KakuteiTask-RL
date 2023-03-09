acc1c14feecbdcd7633b35d10eb22d4227a53a11
Jun <kumejun2000@gmail.com>
2023-03-09 16:37:49 +0900

    Collectorクラスを作成(必要そうなメソッド作成)

---
+++ collector.py
@@ -0,0 +1,24 @@
+class Collector:
+    def __init__(self):
+        pass
+
+    def reset(self):
+        pass
+
+    def collect_episode_data(self):
+        pass
+
+    def save_episode_data(self):
+        pass
+
+    def collect_simulation_data(self):
+        pass
+
+    def save_simulation_data(self):
+        pass
+
+    def save_csv(self):
+        pass
+
+    def save_img(self):
+        pass

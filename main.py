f7456b403ced0b5d417fe8f16a15e3b0f122a680
Jun <kumejun2000@gmail.com>
2023-03-09 15:32:14 +0900

    ハイパーパラメータによってフォルダ名を生成

--- main.py
+++ main.py
@@ -1,23 +1,38 @@
 import os
 from datetime import datetime
+from make_folder import compare_base_make_folder
 from simulator import Simulator
+from q_learning import QLearning
+from q_learning_bias import QLearningBias
 
-def make_folder():
-    log_path = f'log/'
-    os.makedirs(log_path, exist_ok=True)
-    time_now = datetime.now()
-    results_dir_path = f'{log_path}{time_now:%Y%m%d%H%M}/'
-    os.makedirs(results_dir_path, exist_ok=True)
-    return results_dir_path
-
-def main():
-    results_dir_path = make_folder()
-    simulation = 100
-    episode = 10000
-    sim = Simulator(simulation, episode, results_dir_path)
-    sim.run()
 
 if __name__ == '__main__':
-    print('started run')
-    main()
-    print('finished run')
+    algo = 'Qbias' #Q or Qbias
+    simulation = 100
+    episode = 10000
+    alpha = 0.01
+    beta = 5
+    gamma = 0.9
+    bias_weight = 5
+
+    ex_param = {
+        'sim': simulation,
+        'epi': episode,
+        'alpha': alpha,
+        'beta': beta,
+        'gamma': gamma,
+        'biasWeight': bias_weight,
+    }
+
+    if algo == 'Q':
+        policy = QLearning()
+        results_dir_path = compare_base_make_folder(algo, ex_param)
+    elif algo == 'Qbias':
+        policy = QLearningBias()
+        results_dir_path = compare_base_make_folder(algo, ex_param)
+    else:
+        print(f'Not found algorithm {algo}')
+        exit(1)
+
+    sim = Simulator(simulation, episode, results_dir_path)
+    sim.run()

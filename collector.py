3e0d4738c843aeccdb49b526383623e043cd67ef
Jun <kumejun2000@gmail.com>
2023-03-10 11:59:43 +0900

    Collectorにaction_rateを追加

--- collector.py
+++ collector.py
@@ -13,27 +13,28 @@ class Collector:
         self.V_sim = np.zeros((self.epi, 4))
         self.TD_Q_sim = np.zeros((self.epi, 5))
         self.TD_V_sim = np.zeros((self.epi, 4))
-        # self.action_count_sim = np.zeros((self.epi, 2))
+        self.action_count_sim = np.zeros((self.epi, 2))
 
     def reset(self):
         self.Q_epi = np.zeros((self.epi, 5))
         self.V_epi = np.zeros((self.epi, 4))
         self.TD_Q_epi = np.zeros((self.epi, 5))
         self.TD_V_epi = np.zeros((self.epi, 4))
-        # self.action_count_epi = np.zeros((self.epi, 2))
+        self.action_count_epi = np.zeros((self.epi, 2))
 
     def collect_episode_data(self, e, Q, V, TD_Q, TD_V, action):
         self.Q_epi[e] += Q
         self.V_epi[e] += V
         self.TD_Q_epi[e] += TD_Q
         self.TD_V_epi[e] += TD_V
-        # self.action_count_epi[e] += action
+        self.action_count_epi[e, action] += 1
 
     def save_episode_data(self):
         self.Q_sim += self.Q_epi
         self.V_sim += self.V_epi
         self.TD_Q_sim += self.TD_Q_epi
         self.TD_V_sim += self.TD_V_epi
+        self.action_count_sim += self.action_count_epi
 
     def collect_simulation_data(self):
         pass
@@ -43,7 +44,7 @@ class Collector:
         self.V_sim /= self.sim
         self.TD_Q_sim /= self.sim
         self.TD_V_sim /= self.sim
-        # self.action_count_sim /= self.sim
+        self.action_count_sim /= self.sim
         self.save_csv_img()
 
     def save_csv_img(self):
@@ -51,7 +52,7 @@ class Collector:
         np.savetxt(f'{self.results_dir_path}V.csv', self.V_sim, delimiter=',')
         np.savetxt(f'{self.results_dir_path}TD_Q.csv', self.TD_Q_sim, delimiter=',')
         np.savetxt(f'{self.results_dir_path}TD_V.csv', self.TD_V_sim, delimiter=',')
-        # np.savetxt(f'{self.results_dir_path}action_count.csv', self.action_count_sim, delimiter=',')
+        np.savetxt(f'{self.results_dir_path}action_count.csv', self.action_count_sim, delimiter=',')
         if self.is_save_img: self.save_img()
 
     def save_img(self):
@@ -59,7 +60,7 @@ class Collector:
         self.sub_plot('V', self.V_sim)
         self.sub_plot('TD_Q', self.TD_Q_sim)
         self.sub_plot('TD_V', self.TD_V_sim)
-        # self.sub_plot('action rate', self.action_count_sim)
+        self.sub_plot('action_rate', self.action_count_sim)
 
     def sub_plot(self, title, thing):
         figure = plt.figure(figsize=(12, 8))
--- simulator.py
+++ simulator.py
@@ -13,24 +13,19 @@ class Simulator:
         self.env = Environment()
         self.collector = Collector(self.sim, self.epi, self.results_dir_path)
         self.algorithm = policy
-        self.Q = np.zeros((self.sim, self.epi, 5)) #Q_HL, Q_HR, Q_LL, Q_LN, Q_LR
-        self.V = np.zeros((self.sim, self.epi, 4)) #START, LL, LN, LR
-        self.td_error_q = np.zeros((self.sim, self.epi, 5)) #Q_HL, Q_HR, Q_LL, Q_LN, Q_LR
-        self.td_error_v = np.zeros((self.sim, self.epi, 4)) #START, LL, LN, LR
-        self.action_count = np.zeros((self.sim, self.epi, 2)) #HL, HR
 
     def run(self):
         for s in tqdm(range(self.sim)):
             self.collector.reset()
             self.algorithm.reset()
             for e in range(self.epi):
-                state, done = self.env.reset() #state=START, #doneは終端状態=True, Not終端状態=False
+                state, done = self.env.reset()
                 while not done:
                     action = self.algorithm.act(state)
                     next_state, reward, done = self.env.step(action)
                     Q, V, TD_Q, TD_V = self.algorithm.update(state, action, reward, next_state)
                     state = next_state
-                    # if action != 2: self.action_count[s, e, action] = 1
-                self.collector.collect_episode_data(e, Q, V, TD_Q, TD_V, action)
+                    if action != 2: first_action = action
+                self.collector.collect_episode_data(e, Q, V, TD_Q, TD_V, first_action)
             self.collector.save_episode_data()
         self.collector.save_simulation_data()
